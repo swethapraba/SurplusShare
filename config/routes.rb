70414eff57b775d47a08227d9f5c7cb8449371cb
SwethaPraba <swetha.prabakaran@gmail.com>
2017-05-23 09:47:53 -0400

    testingss

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-	#root 'welcome#index' #From Heroku
-	#From TutorialsPoint
-	get "organization", to:"organization#index", as: :organization
-	#get 'organization/list' 
+  	root 'welcome#index' #From Heroku
+	get 'organization/list' #From TutorialsPoint 
 	get 'organization/new'
 	post 'organization/create'
 	patch 'organization/update'
