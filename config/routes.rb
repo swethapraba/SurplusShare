3a299fb1df5b004fee316622f281555ef900b75b
SwethaPraba <swetha.prabakaran@gmail.com>
2017-05-23 10:28:51 -0400

    learning routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	#root 'welcome#index' #From Heroku
-	#root 'organization#list'
+	root 'organization#list'
 	#get 'organization/list' #From TutorialsPoint 
 	get 'organization/new'
 	post 'organization/create'
