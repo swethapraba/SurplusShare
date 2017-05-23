d2e791f0f0b2fba10f13f0c2fd3f055f3d37671a
SwethaPraba <swetha.prabakaran@gmail.com>
2017-05-23 10:06:23 -0400

    working on organization list

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-	get 'organization#list'
+	#root 'welcome#index' #From Heroku
+	root 'organization#list'
 	#get 'organization#list' #From TutorialsPoint 
 	get 'organization/new'
 	post 'organization/create'
@@ -11,5 +12,4 @@ Rails.application.routes.draw do
 	get 'organization/update'
 	get 'organization/show_organizationtypes'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    	#root 'welcome#index' #From Heroku
 end
